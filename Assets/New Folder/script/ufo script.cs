385d2d79d1760d1b58c9ae12496b892d93018b01
Mattheug21 <133687392+Mattheug21@users.noreply.github.com>
2024-03-28 20:13:25 -0400

    Walking person

--- Assets/New Folder/script/ufo script.cs
+++ Assets/New Folder/script/ufo script.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ufoscript : MonoBehaviour
+public class moremovement : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W)){
+            Debug.Log("pressed the w key to move forwards");
+            transform.position += new Vector3(0f,0f,1f);
+        }
+        if (Input.GetKeyDown(KeyCode.S)){
+            Debug.Log("pressed the s key to move backwards");
+            transform.position -= new Vector3(0f,0f,1f);
+        }
+        if (Input.GetKeyDown(KeyCode.A)){
+            Debug.Log("pressed the a key to move to the left");
+            transform.position -= new Vector3(1f,0f,0f);
+        }
+        if (Input.GetKeyDown(KeyCode.D)){
+            Debug.Log("pressed the d key to move to the right");
+            transform.position += new Vector3(1f,0f,0f);
+        }
     }
 }
